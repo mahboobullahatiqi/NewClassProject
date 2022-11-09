45763d6f0291e3d9b0ef7a1777c382fd07890041
mahboob atiqi <mahboobullah.atiqi@tekschool.us>
2022-11-08 18:41:08 -0800

    second commit

---
+++ src/classpractice/NewClassProject.java
@@ -0,0 +1,14 @@
+package classpractice;
+
+public class NewClassProject {
+	public static void main(String[] args) {
+		 int x =2;
+		 int y=82;
+		 System.out.println(x*y);
+		
+	}
+	public void javajan () {
+		System.out.println("java and coding is fan");
+	}
+
+}
